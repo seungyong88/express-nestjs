41177f81536968825db2604f9ddae712378769ef
seungyong88 <gosng2001@naver.com>
2021-11-01 18:01:16 +0900

    미들웨어 추가

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,33 @@ const port: number = 4000;
 
 const data = [1, 2, 3, 4];
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware")
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware22"2)
+  next();
+});
+
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat }) // 캣정보 내려보내주기
 })
+
+app.get('/cats/blue', (req, res, next: express.NextFunction) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ blue: Cat[0] });
+})
+
+app.get('/cats/som', (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ som: Cat[1] });
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
